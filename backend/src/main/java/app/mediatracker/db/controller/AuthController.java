2f77d032592a6fad62fd4c6cdc4f22e5fe140c13
Ploß Alexander <ap153@hdm-stuttgart.de>
2025-12-10 12:11:41 +0100

    add: JWT Token generation and JWT filter to deny unauthorized access#45

---
+++ backend/src/main/java/app/mediatracker/db/controller/AuthController.java
@@ -0,0 +1,33 @@
+package app.mediatracker.db.controller;
+
+import app.mediatracker.db.domain.User;
+import app.mediatracker.db.dto.JwtResponse;
+import app.mediatracker.db.dto.LoginRequest;
+import app.mediatracker.db.service.JwtService;
+import app.mediatracker.db.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    public AuthController(UserService userService, JwtService jwtService) {
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        User user = userService.login(loginRequest.getUsername(), loginRequest.getPassword());
+
+        String token = jwtService.generateToken(user.getUsername());
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+}
+
---
+++ backend/src/main/java/app/mediatracker/db/security/JwtFilter.java
@@ -0,0 +1,48 @@
+package app.mediatracker.db.security;
+
+import app.mediatracker.db.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        String username = null;
+        String token = null;
+
+        if (authHeader !=null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            username = jwtService.extractUsername(token);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if(jwtService.verifyToken(token, username)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
