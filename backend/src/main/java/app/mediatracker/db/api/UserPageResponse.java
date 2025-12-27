d4caeec03284cbd90ece479bbce1ccb2151e23c0
Adrulu <adrianrossa@web.dem>
2025-12-27 12:08:28 +0100

    add: API-Responses/Requests for UserSearch and UserPage

---
+++ backend/src/main/java/app/mediatracker/db/api/UserPageResponse.java
@@ -0,0 +1,24 @@
+package app.mediatracker.db.api;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/**
+ * API-Response für eine einzelne User-Page inklusive zugehöriger MediaItem-Daten 
+ * (falls die Liste auf Public gestellt ist).
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPageResponse {
+
+    private String name;
+
+    private String profilePictureUrl;
+
+    private Boolean publicList;
+
+    //private List<MediaEntry> entries;
+    
+}
---
+++ backend/src/main/java/app/mediatracker/db/api/UserSummary.java
@@ -0,0 +1,24 @@
+package app.mediatracker.db.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Darstellung der User-Basisinformationen für API-Responses der User-Search.
+ * <p>
+ * Dieses DTO spiegelt die Felder wider, die als Snapshot in {@code User}
+ * gespeichert werden.
+ * </p>
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSummary {
+
+    private String name;
+    private String profilePictureUrl;
+    private Boolean publicList;
+}
