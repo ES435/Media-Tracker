cc856a278309f7a7afcd76fcb4157ff661d1ea64
Adrulu <adrianrossa@web.dem>
2025-12-29 17:15:12 +0100

    add: mediaList field for displayal of users media lists on user page

--- backend/src/main/java/app/mediatracker/db/api/UserPageResponse.java
+++ backend/src/main/java/app/mediatracker/db/api/UserPageResponse.java
@@ -1,4 +1,6 @@
 package app.mediatracker.db.api;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +21,6 @@ public class UserPageResponse {
 
     private Boolean publicList;
 
-    //private List<MediaEntry> entries;
-    
+    private List<LibraryEntryResponse> mediaList;
+
 }
